06266f04fe2e13f6c8ab09b0d4f580a0f943c446
juncheng785245941 <785245941@qq.com>
2016-05-14 22:22:27 +0800

    Initial Commit

---
+++ CJCImageViewPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJCImageViewPicker
+//
+//  Created by 陈俊城 on 16/5/14.
+//  Copyright © 2016年 陈俊城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CJCImageViewPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CJCImageViewPicker
+//
+//  Created by 陈俊城 on 16/5/14.
+//  Copyright © 2016年 陈俊城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
